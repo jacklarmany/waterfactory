4d956d7834357db496adb1c638708a5975152f4d
JACK LARMANY <jacklarmany@gmail.com>
2021-12-07 06:02:50 +0700

    frontend

--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -14,19 +14,19 @@ $this->params['breadcrumbs'][] = $this->title;
     <div class="row">
         <div class="col-md-4"></div>
         <div class="col-md-4">
-            <div class="site-signup">
+            <div class="site-signup p-4 bg-white shadow border ">
                 <h1><?= Html::encode($this->title) ?></h1>
                 <p>Please fill out the following fields to signup:</p>
-
-                <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-                <?= $form->field($model, 'email') ?>
-                <?= $form->field($model, 'password')->passwordInput() ?>
-                <div class="form-group">
+                <br>
+                <?php $form = ActiveForm::begin(['id' => 'form-signup']) ?>
+                <?= $form->field($model, 'username')->textInput(['autofocus' => false, 'type'=>'text', 'placeholder' => Yii::t('app','Username')])->label(false); ?>
+                <?= $form->field($model, 'email')->textInput(['type'=>'email', 'placeholder' => Yii::t('app','Email')])->label(false) ?>
+                <?= $form->field($model, 'password')->passwordInput(['type'=>'password', 'placeholder' => Yii::t('app', 'Password')])->label(false); ?>
+                <div class="form-group text-center">
                     <?= Html::submitButton('Signup', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
                 </div>
                 <?php ActiveForm::end(); ?>
-                
+
             </div>
         </div>
         <div class="col-md-4"></div>
